358eba3af1d2b62cce827373e2d4182f2023d7ec
Ihar-Batura <A17331582@gmail.com>
2023-12-12 13:44:06 +0300

    feat: add code to carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let c = 0.693 / HALF_LIFE_PERIOD;
+  let a = sampleActivity;
+  let b = +sampleActivity;
+  if (typeof a === 'number' || typeof a === 'object' || b > 15 || b <= 0 || Number.isNaN(b)) {
+    return false;
+  } else {
+    return Math.ceil(Math.log((MODERN_ACTIVITY/sampleActivity)) / c);
+  }
+  
 }
 
 module.exports = {
